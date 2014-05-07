eb90ff4f7d4b51eaef773068fa5155cbc71dc414
Essam Joubori <essam.joubori@gmail.com>
2014-05-06 19:28:57 -0500

    Created product table database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ECommerce::Application.config.secret_key_base = 'a52b616d0ea4b019a2b3e7e5e3248bf82352c4c3639f260c93a4cf8df14735c678b05a3ca58f2425ac84d1f0149e1e02d9766d56562c71a006cf37bfd5fc91dd'
