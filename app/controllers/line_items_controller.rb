7096d4b548e77d995deb46ab61c8e2ad0dd5aab7
Essam Joubori <essam.joubori@gmail.com>
2014-05-08 11:01:34 -0500

    Finalized cart and cart#show controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,10 +29,10 @@ class LineItemsController < ApplicationController
 
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-
+    @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
+        format.html { redirect_to @line_item.cart }
         format.html { redirect_to @line_item.cart,
           notice: 'Line item was successfully created.' }
         format.json { render action: 'show',
@@ -77,7 +77,7 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
     
